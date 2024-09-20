026a03bbb0a279c4cae3843ae8bd8c727d0a7987
biranudiriba2024 <biranudiriba2024@gmail.com>
2024-09-20 14:00:35 +0300

    userCorrected

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,14 @@
-import User from "../models/Users.js";
-import { validateUser } from "../models/Users.js";
-
+// import User from "../models/Users.js";
+// import { validateUser } from "../models/Users.js";
+import Users from "../models/Users.js";
 export default {
   createUser: async (req, res) => {
     const { error, value } = validateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    const user = await User.findOne({ email: req.body.email });
+    const user = await Users.findOne({ email: req.body.email });
     if (user) return res.send(user?.generateAuthToken());
 
-    const newUser = new User({
+    const newUser = new Users({
       ...value,
     });
     // const salt = await bcrypt.genSalt(10);
@@ -24,9 +24,9 @@ export default {
     res.send(newUser.generateAuthToken());
   },
   getUser: async (req, res) => {
-    const { userId } = req.params;
-    console.log(userId);
-    const user = await User.findById(userId);
+    const { userid } = req.params;
+    console.log(userid);
+    const user = await Users.findById(userid);
     console.log(user);
     if (!user) return res.status(404).send(null);
 
